8f43be500ca7420c993bb260d980cc6c41ff2012
carlosbarradas20 <carlosbarradasaleman30@gmail.com>
2023-06-20 19:40:16 -0600

    segunda pre entrega

--- codigo.js
+++ codigo.js
@@ -1,51 +1,81 @@
-class Calculadora{
-    constructor(){
 
+// calculadora
+let historial = [];
+
+// Función para realizar una operación
+function realizarOperacion(operacion) {
+    let num1 = parseFloat(prompt("Ingrese el número 1"));
+    let num2 = parseFloat(prompt("Ingrese el número 2"));
+    let resultado;
+
+    switch (operacion) {
+        case 1: // Suma
+            resultado = num1 + num2;
+            alert("Resultado de la suma: " + resultado);
+            historial.push({ operacion: 'suma', resultado: resultado });
+            break;
+        case 2: // Resta
+            resultado = num1 - num2;
+            alert("Resultado de la resta: " + resultado);
+            historial.push({ operacion: 'resta', resultado: resultado });
+            break;
+        case 3: // Multiplicación
+            resultado = num1 * num2;
+            alert("Resultado de la multiplicación: " + resultado);
+            historial.push({ operacion: 'multiplicacion', resultado: resultado });
+            break;
+        case 4: // División
+            resultado = num1 / num2;
+            alert("Resultado de la división: " + resultado);
+            historial.push({ operacion: 'division', resultado: resultado });
+            break;
+        default:
+            alert("Operación no válida");
+            return;
     }
-sumar(num1,num2){
-   return parseInt(num1) + parseInt(num2);
-}
-restar(num1,num2){
-   return parseInt(num1) - parseInt(num2);
-}
-dividir(num1,num2){
-   return parseInt(num1) / parseInt(num2);
-}
-multiplicar(num1,num2){
-   return parseInt(num1) * parseInt(num2);
+
+    mostrarHistorial();
 }
 
+// Función para mostrar el historial
+function mostrarHistorial() {
+    let historialTexto = '';
+    for (let i = 0; i < historial.length; i++) {
+        historialTexto += 'Operación: ' + historial[i].operacion + ', Resultado: ' + historial[i].resultado + '\n';
+    }
+    alert("Historial:\n" + historialTexto);
 }
 
-const calculadora = new Calculadora()
+// Función para filtrar el historial por operación
+function filtrarHistorial(operacion) {
+    let historialFiltrado = historial.filter(function(item) {
+        return item.operacion === operacion;
+    });
 
-alert("¿que operacion deseas realizar?");
-let operacion = prompt("1: suma, 2: restar 3: dividir 4: multiplicar");
+    let historialTexto = '';
+    for (let i = 0; i < historialFiltrado.length; i++) {
+        historialTexto += 'Operación: ' + historialFiltrado[i].operacion + ', Resultado: ' + historialFiltrado[i].resultado + '\n';
+    }
+    alert("Historial filtrado:\n" + historialTexto);
+}
 
-if (operacion == 1){
-   let numero1 = prompt("primer numero para sumar");
-   let numero2 = prompt("segundo numero para sumar");
-   resultado = calculadora.sumar(numero1,numero2);
-   alert(`tu resultado es ${resultado}`);
+// Bucle para seguir realizando operaciones
+let continuar = true;
+while (continuar) {
+    // Obtener la operación seleccionada por el usuario
+    let opcion = parseInt(prompt("Seleccione una operación:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División"));
+
+    // Realizar la operación seleccionada
+    realizarOperacion(opcion);
+
+    // Preguntar al usuario si desea continuar
+    let respuesta = prompt("¿Desea realizar otra operación? (S/N)").toUpperCase();
+    if (respuesta !== "S") {
+        continuar = false;
+    } 
 }
-else if (operacion == 2) {
-   let numero1 = prompt("primer numero para restar");
-   let numero2 = prompt("segundo numero para restar");
-   resultado = calculadora.restar(numero1,numero2);
-   alert(`tu resultado es ${resultado}`);
-}
-else if (operacion == 3) {
-   let numero1 = prompt("primer numero para dividir");
-   let numero2 = prompt("segundo numero para dividir");
-   resultado = calculadora.dividir(numero1,numero2)
-   alert(`tu resultado ${resultado}`);
-}
-else if (operacion == 4) {
-   let numero1 = prompt("primer numero para multiplicar");
-   let numero2 = prompt("segundo numero para multiplicar");
-   resultado = calculadora.multiplicar(numero1,numero2)
-   alert(`tu resultado ${resultado}`);
-}
-else {
-   alert("no se ha encontrado la operacion")
-}
+// Obtener la operación para filtrar el historial
+let opcionFiltro = prompt("Seleccione una operación para filtrar el historial:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División");
+
+// Filtrar y mostrar el historial según la operación seleccionada
+filtrarHistorial(opcionFiltro);
